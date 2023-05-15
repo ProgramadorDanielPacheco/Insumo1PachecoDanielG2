1e52ef9109f5b2abb996f4587ddc845f2e49e4bb
ArielCalleSteven <a.calleduma123@gmail.com>
2023-05-15 12:17:16 -0500

    Base_De_Datos

---
+++ src/app/animales.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+    nombre: string;
+    tipo: string;
+    alimentacion: string;
+    descripcion: string;
+    tama: string;
+    adaptabilidad: string;
+    longevidad: string;
+    salud: string;
+    imagen: string;
+  }
+  
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
-import { BdService } from './../../Services/bd.service';
+import { BdService } from '../../Services/bd.service';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Item } from 'src/Models/item';
 import { FormsModule } from "@angular/forms";;
-import { ToastService } from 'src/Services/toast.service';
-import { LoadingService } from 'src/Services/loading.service';
 import { CommonModule } from '@angular/common';
+import { Animal } from '../comida';
+
+
 
 @Component({
   selector: 'app-home',
@@ -16,52 +17,16 @@ import { CommonModule } from '@angular/common';
 })
 export class HomePage {
   private enlace:string = 'Alimentos';
-  public Alimentos:Item[]=[];
-  public newAlimento:Item={
-    nombre: '',
-    precio: '',
-    calificacion: '',
-    id: '',
-    imagen: ''
-  };
-  constructor(private bd:BdService, private toast:ToastService, private load:LoadingService) {
+  comida: Animal[]=[]
+  
+ 
+  constructor(private bd:BdService) {
   }
   ngOnInit() {
-    this.bd.get<Item>(this.enlace).subscribe(p=>{
-      this.Alimentos=p;
-    });
-  }
-  save(){
-    this.load.presentLoading();
-    this.newAlimento.id=this.bd.createId(this.enlace);
-    const data = this.newAlimento;
-    this.bd.add<Item>(data,this.enlace,this.newAlimento.id).then(()=>{
-      this.toast.showToast("Exito al guardar","success","checkbox-outline");
-      this.load.dismissLoading();
-      this.clean();
-    }).catch(()=>{
-      this.toast.showToast("Error al guardar","danger","sad-outline");
-    });
-  }
-
-  delete(p:Item){
-    this.load.presentLoading();
-    this.bd.delete(`Personas`,p.id).then(()=>{
-      this.toast.showToast("Exito al Borrar","success","trash-outline");
-      this.load.dismissLoading();
-    }).catch(()=>{
-      this.toast.showToast("Error al Borrar","danger","sad-outline");
+    this.bd.get<Animal>(this.enlace).subscribe(p=>{
+      this.comida=p;
     });
 
   }
-
-  clean(){
-    this.newAlimento.id="";
-    this.newAlimento.calificacion="";
-    this.newAlimento.nombre="";
-    this.newAlimento.precio="";
-    this.newAlimento.imagen="";
-    }
-  }
-
+}
 
